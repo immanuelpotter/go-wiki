3d406fbefa8f4d9133dce0d442788419a1fbb2f9
immanuelpotter <mannypotter@hotmail.com>
2018-11-30 16:58:29 +0000

    Added HTML templates, Removed duplication for rendering templates

--- wiki.go
+++ wiki.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
     "io/ioutil"
     "net/http"
+    "html/template"
     "log"
 )
 
@@ -15,6 +15,11 @@ type Page struct {
 func (p *Page) save() error {
     filename := p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -29,7 +34,7 @@ func loadPage(title string) (*Page, error) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div", p.Title, p.Body)
+    renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request){
@@ -38,12 +43,7 @@ func editHandler(w http.ResponseWriter, r *http.Request){
     if err != nil {
         p = &Page{Title: title}
     }
-    fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-        "<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-        p.Title, p.Title, p.Body)
+    renderTemplate(w, "edit", p)
 }
 
 func main(){
